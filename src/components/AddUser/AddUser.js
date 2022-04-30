a7a8cceee5a3de6499ad42310ffeccac7a5f2937
khadiza21 <bibikhadiza474@gmail.com>
2022-04-30 22:45:14 +0600

    react node mongodb crud client

--- src/components/AddUser/AddUser.js
+++ src/components/AddUser/AddUser.js
@@ -3,7 +3,7 @@ import React from 'react';
 const AddUser = () => {
     return (
         <div>
-            <h3>add user</h3>
+            <h3>please add a new user</h3>
         </div>
     );
 };
