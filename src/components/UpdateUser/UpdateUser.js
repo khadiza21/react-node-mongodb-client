2c577bb903c6bae3f8d843fa74d1ec308489edc3
khadiza21 <bibikhadiza474@gmail.com>
2022-05-02 00:02:09 +0600

    react node mongodb crud client

--- src/components/UpdateUser/UpdateUser.js
+++ src/components/UpdateUser/UpdateUser.js
@@ -9,10 +9,40 @@ const UpdateUser = () => {
     fetch(url)
       .then((res) => res.json())
       .then((data) => setUser(data));
-  }, []);
+  }, [id]);
+
+  const handleUpdateUser = (event) => {
+    event.preventDefault();
+
+    const name = event.target.name.value;
+    const email = event.target.email.value;
+
+    const updateUser = { name, email };
+
+    const url = `http://localhost:5000/user/${id}`;
+    fetch(url, {
+      method: "PUT", // or 'PUT'
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(updateUser),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("success", data);
+        alert("users added successfully!");
+        event.target.reset();
+      });
+  };
+
   return (
     <div>
       <h3>Updating User: {user.name}</h3>
+      <form onSubmit={handleUpdateUser}>
+        <input type="text" name="name" placeholder="Name" required /> <br />
+        <input type="email" name="email" placeholder="Email" required /> <br />
+        <input type="submit" value="Update User" />
+      </form>
     </div>
   );
 };
