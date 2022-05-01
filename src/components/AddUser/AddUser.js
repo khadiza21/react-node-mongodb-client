e1f6f687894760fadd30a44ed9efff1ecb52a631
khadiza21 <bibikhadiza474@gmail.com>
2022-05-01 13:22:13 +0600

    react node mongodb crud client

--- src/components/AddUser/AddUser.js
+++ src/components/AddUser/AddUser.js
@@ -1,11 +1,38 @@
-import React from 'react';
+import React from "react";
 
 const AddUser = () => {
-    return (
-        <div>
-            <h3>please add a new user</h3>
-        </div>
-    );
+  const handleAddUser = (event) => {
+    event.preventDefault();
+
+    const name = event.target.name.value;
+    const email = event.target.email.value;
+
+    const user = { name, email };
+    
+
+    fetch("http://localhost:5000/user ", {
+      method: "POST", // or 'PUT'
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })    
+      .then(res => res.json())
+      .then(data => {
+          console.log('success ',data);
+      })
+  };
+
+  return (
+    <div>
+      <h3>please add a new user</h3>
+      <form onSubmit={handleAddUser}>
+        <input type="text" name="name" placeholder="Name" required /> <br />
+        <input type="email" name="email" placeholder="Email" required /> <br />
+        <input type="submit" value="Add User" />
+      </form>
+    </div>
+  );
 };
 
 export default AddUser;
